86d1dd64df27392c37b369fb4f4012ff903eabb3
Kaylie Son <kayliemoa@gmail.com>
2018-02-24 15:41:55 -0800

    login
    with Sehee

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "bloom_user"; 
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if(isset($_POST["userkey"]) && !empty($_POST["userkey"]))
+{
+
+    $userkey = $_POST['userkey'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    
+    $sql_userkey = "SELECT * FROM users WHERE userkey = '$userkey'";
+    
+    $res_u = mysqli_query($conn, $sql_userkey);
+    
+    if(mysqli_num_rows($res_u) > 0){
+        
+    }else{ $sql = "insert into users (userkey,name,email) VALUES ('$userkey','$name','$email')";
+    
+        $result = mysqli_query($conn, $sql);
+        $conn->close();
+            }
+       } 
+    
+
+else {
+    echo ("false");
+    }
+
+?>
