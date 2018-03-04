7c862e62695462b01ca826bf06e6957cc14cad06
SE HEE AHN <seheeahn@ses-macbook-pro.wifi.bcit.ca>
2018-03-03 17:16:05 -0800

    1231
    231231231

--- quotes_adding.php
+++ quotes_adding.php
@@ -5,8 +5,9 @@
         <textarea type="text" class="form-control" placeholder="Type your text here" aria-label="Recipient's username" aria-describedby="basic-addon2" id="reviewtext" name="reviewtext" rows="3" cols="40" placeholder="type your text here"></textarea>
         <input type="hidden" name="reviewdate" id="reviewdate">
         <br/>
-    </div>
-    <div class="input-group-append">
+        <div class="input-group-append">
         <input type="submit" class="btn btn-outline-secondary" value="Add">
     </div>
+    </div>
+    
 </form>
